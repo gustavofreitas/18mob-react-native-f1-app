0e3473dab4296f52b1c9071e3f308e2d08943941
Danilo Santucci <dansantucci@live.com>
2019-12-22 21:42:39 -0300

    feat(Detail) Ajustes na tela de Detalhes

--- f1-app/screen/detail/components/constructor.js
+++ f1-app/screen/detail/components/constructor.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Text } from 'native-base';
+
+const styles = StyleSheet.create({
+    center: {
+        textAlign: "center",
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 20,
+        textAlign: "center",
+        marginTop: 20,
+        marginBottom: 10
+    },
+    descr: {
+        fontSize: 20,
+        textAlign: "center",
+        marginLeft: 10
+    }
+}); 
 
 const Constructor = (props) => {
     console.log(props);
@@ -8,9 +28,16 @@ const Constructor = (props) => {
         let constructor = props.constructor;
         if (constructor != null && Object.keys(constructor).length != 0) {
             return (
-                <View>
-                    <Text>{`Nome: ${constructor.name}`}</Text>
-                    <Text>{`Nacionalidade: ${constructor.nationality}`}</Text>
+                <View style={ styles.center  }>
+                    <Text style={ styles.title }>Nome:</Text>
+                    <Text style={ styles.descr }>
+                        {constructor.name}
+                    </Text>
+
+                    <Text style={ styles.title }>Nacionalidade:</Text>
+                    <Text style={ styles.descr }>
+                        {constructor.nationality}
+                    </Text>
                 </View>
             );
         }
--- f1-app/screen/detail/components/race.js
+++ f1-app/screen/detail/components/race.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Text } from 'native-base';
+
+const styles = StyleSheet.create({
+    center: {
+        textAlign: "center",
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 20,
+        textAlign: "center",
+        marginTop: 20,
+        marginBottom: 10
+    },
+    descr: {
+        fontSize: 20,
+        textAlign: "center",
+        marginLeft: 10
+    }
+}); 
 
 const Race = (props) => {
     console.log(props);
@@ -8,11 +28,26 @@ const Race = (props) => {
         let race = props.race;
         if (race != null && Object.keys(race).length != 0) {
             return (
-                <View>
-                    <Text>{`Nome: ${race.raceName}`}</Text>
-                    <Text>{`Nome: ${race.Circuit.circuitName}`}</Text>
-                    <Text>{`Local: ${race.Circuit.Location.locality}`}</Text>
-                    <Text>{`Pais: ${race.Circuit.Location.country}`}</Text>
+                <View style={ styles.center  }>
+                    <Text style={ styles.title }>Nome da Corrida:</Text>
+                    <Text style={ styles.descr }>
+                        {race.raceName}
+                    </Text>
+
+                    <Text style={ styles.title }>Nome do Circuito:</Text>
+                    <Text style={ styles.descr }>
+                        {race.Circuit.circuitName}
+                    </Text>
+
+                    <Text style={ styles.title }>Local:</Text>
+                    <Text style={ styles.descr }>
+                        {race.Circuit.Location.locality}
+                    </Text>
+
+                    <Text style={ styles.title }>País:</Text>
+                    <Text style={ styles.descr }>
+                        {race.Circuit.Location.country}
+                    </Text>
                 </View>
             );
         }
